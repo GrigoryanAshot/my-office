9a40e6c59e78e291333df6f91d9d6f45bf33b973
Ashot Grigoryan <ashotgrigoryan94@gmail.com>
2025-06-01 02:29:42 +0400

    Trigger production deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { Provider } from "react-redux";
 import store from "@/redux/store"; // Make sure this path is correct
 import "../public/css/nav.css";
 import type { AppProps } from 'next/app';
-
+// trigger rebuild
 export default function MyApp({ Component, pageProps }: AppProps) {
   return ( 
     <Provider store={store}>
