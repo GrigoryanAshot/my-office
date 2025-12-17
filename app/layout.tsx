80d5e154408facdcc61a054326138d92823d715e
Ashot Grigoryan <ashotgrigoryan94@gmail.com>
2025-12-17 18:51:23 +0400

    Fix CSS build error: Remove @import statements (not supported in Next.js 16/Turbopack), add fonts to layout.tsx instead

--- app/layout.tsx
+++ app/layout.tsx
@@ -109,6 +109,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800;900&family=Rubik:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="alternate" hrefLang="en" href="https://www.my-office.am/en" />
         <link rel="alternate" hrefLang="hy" href="https://www.my-office.am/hy" />
